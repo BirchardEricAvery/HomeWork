a41796825fa379d99f15378ee8f5ec030043e66c
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-01 20:52:27 -0700

    boolean usage

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -9,14 +9,16 @@ Instructor: Dan Williams
 //numbers, strings, boolean--skipped, too basic
 //declare before defining
 
-var yearBorn = 1969;    //var string, declaring variables
-var age;                //age variable declared
-age = 46                //defining age
+
+var yearBorn = 1969;        //var string, declaring variables
+var age;                    //age variable declared
+var eAveryBirchard = true;  //boolean making eAveryBirchard true
+age = 46                    //defining age
 
 //or var yearBorn = 46
 
 alert("testing alert for yearBorn" + " " + yearBorn + " " + age);
 
 console.log("yearBorn variable" + " " + yearBorn + " " + age); //log yearBorn variable
-
+console.log("eAveryBirchard value is" + " " + eAveryBirchard);
 
