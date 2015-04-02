15cc61366b1ee4ce7c30e379951a0675960e7842
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-01 20:33:25 -0700

    Console.log

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -1,1 +1,17 @@
-alert("This is in the js file");
+/*
+Name:       Eric Avery Birchard
+Course:     SDI
+Date:       1, April 2015
+Instructor: Dan Williams
+
+ */
+
+//numbers, strings, boolean--skipped, too basic
+
+var yearBorn = 1955;  //var string
+
+alert("testing alert for yearBorn" + " " + yearBorn);
+
+console.log("yearBorn variable" + " " + yearBorn); //log yearBorn variable
+
+
