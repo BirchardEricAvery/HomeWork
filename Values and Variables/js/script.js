77400e0597eb9bee14f034fd4a2ee14693972f20
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-01 21:14:51 -0700

    Super basic arrays
    Getting a better clue now

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -13,12 +13,20 @@ Instructor: Dan Williams
 var yearBorn = 1969;        //var string, declaring variables
 var age;                    //age variable declared
 var eAveryBirchard = true;  //boolean making eAveryBirchard true
+var info = ["Avery", "was", "born", yearBorn]; //making array
+info[4] = "end array";
 age = 46                    //defining age
 
 //or var yearBorn = 46
 
-alert("testing alert for yearBorn" + " " + yearBorn + " " + age);
+alert("testing alert for yearBorn\n" + " " + yearBorn + " " + age);
 
-console.log("yearBorn variable" + " " + yearBorn + " " + age); //log yearBorn variable
-console.log("eAveryBirchard value is" + " " + eAveryBirchard);
+console.log("yearBorn variable \n" + " " + yearBorn + " " + age); //log yearBorn variable
+console.log("eAveryBirchard value is \n" + " " + eAveryBirchard); //log eAveryBirchard value
+console.log(info); //displaying array
 
+
+//array access notation
+
+console.log("Array access notation in position 3" + " " + info[3]);
+console.log(info[4]);
