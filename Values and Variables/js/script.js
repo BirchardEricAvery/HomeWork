1647af0036dcf9a96c02b4ddf13e963a6684f361
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-03 10:31:32 -0700

    Variable homework

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -30,3 +30,26 @@ console.log(info); //displaying array
 
 console.log("Array access notation in position 3" + " " + info[3]);
 console.log(info[4]);
+
+//Two-Dimensional array
+
+var personnel = new Array();
+
+personnel[0] = new Array();
+personnel[0][0] = "Name0";
+personnel[0][1] = "Age0";
+personnel[0][2] = "Address0";
+
+personnel[1] = new Array();
+personnel[1][0] = "Name1";
+personnel[1][1] = "Age1";
+personnel[1][2] = "Address1";
+
+personnel[2] = new Array();
+personnel[2][0] = "Name2";
+personnel[2][1] = "Age2";
+personnel[2][2] = "Address2";
+
+console.log("Name : " + personnel[0][0] + personnel[1][0] + personnel[2][0] + "\r");
+console.log("Age : " + personnel[0][1] + personnel[1][1] + personnel[2][1] + "\r");
+console.log("Address : " + personnel[0][2] + personnel[1][2] + personnel[2][2] + "\r");
