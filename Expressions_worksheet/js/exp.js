e9df74971399722685526776df0d8037e72ad742
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-03 19:13:24 -0700

    Working on Groceries

---
+++ Expressions_worksheet/js/exp.js
@@ -0,0 +1,98 @@
+/*
+ Name:       Eric (Avery) Birchard
+ Course:     SDI
+ Date:       3, April 2015
+ Instructor: Dan Williams
+
+
+
+//Calculate how old Sparky the pit bull is in dog years.
+
+//Sparky's age
+
+var ageDog; //initial age variable
+
+var ageDog = prompt("What is Sparky's' age?");  //gathering input for dog age via user input
+var ageDog = Number(ageDog);                    //Casting string to number
+var age;                                        //variable for human years
+
+var age = ageDog;  //putting human years variable into age for output of human years
+
+ageDog = ageDog * 7; //calculation for dog years
+
+
+alert("Sparky's' age in human years is" + " "   //user reads age in human years
++ age + " " + "\n" + "which is" + " "           //displays human years
++ ageDog + " " + "in dog years");               //user reads age in dog years
+
+console.log("Sparky's age in dog years is:" + " " + ageDog + "\n"); //console log for dog years
+console.log("Sparky's age in human years:" + " " + age);            //console log for human years
+
+
+//Slice of pie I, no it's pizza.  Title is Pie.
+
+//Party Pizza
+
+var slicesPizza = prompt("How many slices of pizza are available?"); //variable for how many slices of pizza
+var slicesPizza = Number(slicesPizza);
+
+var numberPeople = prompt("Total number of people who want pizza?"); //variable for how many people
+var numberPeople = Number(numberPeople);
+
+var result = slicesPizza / numberPeople;                        //result of how many slices per person
+
+
+alert("Slices of pizza available per person:" + " " + result);  //display result for how many slices per person
+
+
+console.log("Slices per person available" + " " + result);      //console log how many slices per person
+
+//Sparky's slices of pizza
+
+var leftOvers = prompt("How many slices were eaten");           //Slices eaten
+var leftOvers = Number(leftOvers);                              //Left overs as a number
+console.log("Slices eaten" +" " + leftOvers);                   //Number of slices eaten
+
+
+var leftOvers = slicesPizza - leftOvers;                                //Left Overs
+
+alert("Sparky gets" + " " + leftOvers + " " + "slices of pizza");       //What Sparky got
+console.log("Sparky got" + " " + leftOvers + " " + "slices of pizza");  //Logging what sparky got
+
+    */
+
+//Groceries
+
+//5 totals of original price into array with description
+
+var grocery1 = prompt("Item description week 1");
+var grocery11 = prompt("Total cost in week 1");
+var grocery11 = number(grocery11);
+
+var grocery2 = prompt("Item description week 2");
+var grocery11 = prompt("Total cost in week 2");
+var grocery22 = number(grocery22);
+
+var grocery3 = prompt("Item description week 3");
+var grocery11 = prompt("Total cost in week 3");
+var grocery33 = number(grocery33);
+
+var grocery4 = prompt("Item description week 4");
+var grocery11 = prompt("Total cost in week 4");
+var grocery44 = number(grocery44);
+
+var grocery5 = prompt("Item description week 5");
+var grocery11 = prompt("Total cost in week 5");
+var grocery55 = number(grocery55);
+
+
+
+Array = newArray
+//output
+
+//After 20% discount
+//Is now $ before tax
+//Price after tax
+
+//
+
