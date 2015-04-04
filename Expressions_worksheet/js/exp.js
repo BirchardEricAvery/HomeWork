b53cb89a0c2327bca5a3c0ea517ab4860765f60f
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-03 22:03:45 -0700

    Pre Array week 2 homework

--- Expressions_worksheet/js/exp.js
+++ Expressions_worksheet/js/exp.js
@@ -64,56 +64,90 @@ console.log("Sparky got" + " " + leftOvers + " " + "slices of pizza");  //Loggin
 //Groceries
 
 //5 totals of original price into array with description
-var discount = 20;      //20% discount
-var tax = 5;            //5% sales tax
-
-var grocery1 = prompt("Item description week 1");       //item description
-var grocery11 = prompt("Total cost in week 1");         //total cost
-var grocery11 = number(grocery11);                      //casting
-var discount1;                                          //discount
-var bTax1;                                              //before tax
-var aTax1;                                              //after tax
-
-var grocery2 = prompt("Item description week 2");       //item description
-var grocery11 = prompt("Total cost in week 2");         //total cost
-var grocery22 = number(grocery22);                      //casting
-var discount2;                                          //discount
-var bTax2;                                              //before tax
-var aTax2;                                              //after tax
-
-var grocery3 = prompt("Item description week 3");       //item description
-var grocery11 = prompt("Total cost in week 3");         //total cost
-var grocery33 = number(grocery33);                      //casting
-var discount3;                                          //discount
-var bTax3;                                              //before tax
-var aTax3;                                              //after tax
-
-var grocery4 = prompt("Item description week 4");       //item description
-var grocery11 = prompt("Total cost in week 4");         //total cost
-var grocery44 = number(grocery44);                      //casting
-var discount4;                                          //discount
-var bTax4;                                              //before tax
-var aTax4;                                              //after tax
-
-var grocery5 = prompt("Item description week 5");       //item description
-var grocery11 = prompt("Total cost in week 5");         //total cost
-var grocery55 = number(grocery55);                      //casting
-var discount5;                                          //discount
-var bTax5;                                              //before tax
-var aTax5;                                              //after tax
+var discount = .20;      //20% discount
+var tax = .05;          //5% sales tax
 
 
-//Calculations
+//Groceries #1
+var description1 = prompt("Item description week 1");   //item description
+var cost1 = prompt("Total cost in week 1");             //total cost
+var cost1 = Number(cost1);                              //casting
+var discount1 = cost1 - (cost1 * discount);             //discount
+
+console.log(description1 + " " + "before tax after discount" + " " + "$" +  "$" + discount1);    //before tax after discount
+
+var aTax1 = discount1 * tax;                                                                    //after tax calculation
+console.log("aTax1" + " " + aTax1);                                                             //logging tax
+
+var aTaxFinal1 = discount1 - aTax1;                                                             //Final tax calc
+console.log(description1 + " " + "Cost after tax and discount" + " " +  "$" + aTaxFinal1);      //after tax and discount logging
 
 
-//Output arrays
 
-Array = newArray
+//Groceries #2
+var description2 = prompt("Item description week 2");       //item description
+var cost2 = prompt("Total cost in week 2");                 //total cost
+var cost2 = Number(cost2);                                  //casting
+var discount2 = cost2 - (cost2 * discount);                 //discount
+
+console.log(description2 + " " + "before tax after discount" + " " +  "$" + discount2);     //before tax after discount
+
+var aTax2 = discount2 * tax;                                                                //after tax calculation
+console.log("aTax2" + " " + aTax2);                                                         //logging tax
+
+var aTaxFinal2 = discount2 - aTax2;                                                             //Final tax calc
+console.log(description2 + " " + "Cost after tax and discount" + " " +  "$" + aTaxFinal2);      //after tax and discount logging
+
+//Groceries #3
+var description3 = prompt("Item description week 3");       //item description
+var cost3 = prompt("Total cost in week 3");                 //total cost
+var cost3 = Number(cost3);                                  //casting
+var discount3 = cost3 - (cost3 * discount);                 //discount
+
+console.log(description3 + " " + "before tax after discount" + " " +  "$" + discount3);     //before tax after discount
+
+var aTax3 = discount3 * tax;                                                                //after tax calculation
+console.log("aTax3" + " " + aTax3);                                                         //logging tax
+
+var aTaxFinal3 = discount3 - aTax3;                                                             //Final tax calc
+console.log(description3 + " " + "Cost after tax and discount" + " " +  "$" + aTaxFinal3);      //after tax and discount logging
 
 
-//After 20% discount
-//Is now $ before tax
-//Price after tax
+//Groceries #4
+var description4 = prompt("Item description week 4");       //item description
+var cost4 = prompt("Total cost in week 4");                 //total cost
+var cost4 = Number(cost4);                                  //casting
+var discount4 = cost4 - (cost4 * discount);                 //discount
 
-//
+console.log(description4 + " " + "before tax after discount" + " " +  "$" + discount4);    //before tax after discount
+
+var aTax4 = discount4 * tax;                                                                //after tax calculation
+console.log("aTax4" + " " + aTax4);                                                         //logging tax
+
+var aTaxFinal4 = discount4 - aTax4;                                                             //Final tax calc
+console.log(description4 + " " + "Cost after tax and discount" + " " +  "$" + aTaxFinal4);      //after tax and discount logging
+
+
+//Groceries #5
+var description5 = prompt("Item description week 5");           //item description
+var cost5 = prompt("Total cost in week 5");                     //total cost
+var cost5 = Number(cost5);                                      //casting
+var discount5 = cost5 - (cost5 * discount);                     //discount
+
+console.log(description5 + " " + "before tax after discount" + " " +  "$" + discount5);     //before tax after discount
+
+var aTax5 = discount5 * tax;                                                                //after tax calculation
+console.log("aTax5" + " " + aTax5);                                                         //logging tax
+
+var aTaxFinal5 = discount5 - aTax5;                                                             //Final tax calc
+console.log(description5 + " " + "Cost after tax and discount" + " " +  "$" + aTaxFinal5);      //after tax and discount logging
+
+
+//Output for arrays
+
+
+
+
+
+
 
